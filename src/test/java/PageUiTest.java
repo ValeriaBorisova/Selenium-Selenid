b3f97d97e710422beb38d73dd5ce45b91b2744a3
Borisova Valeria <cerveau305@gmail.com>
2022-01-16 22:40:53 +0500

    init commit

--- src/test/java/PageUiTest.java
+++ src/test/java/PageUiTest.java
@@ -1,50 +1,21 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class PageUiTest {
-    WebDriver driver;
-
-    @BeforeAll
-    public static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    public void setupTest() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-    public void teardown() {
-        driver.quit();
-        driver = null;
-    }
 
     @Test
     public void shouldSubmitRequest() {
-        driver.get("http://localhost:9999/");
-        WebElement feedbackFormName = driver.findElement(By.cssSelector("[data-test-id='name']"));
-        feedbackFormName.findElement(By.className("input__control")).sendKeys("Сергей Королев");
-        WebElement feedbackFormPhone = driver.findElement(By.cssSelector("[data-test-id='phone']"));
-        feedbackFormPhone.findElement(By.className("input__control")).sendKeys("+79574355580");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.tagName("button")).click();
-        String expectedText = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
-        assertEquals(expectedText, actualText);
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Сергей Павлович Королев ");
+        form.$("[data-test-id=phone] input").setValue("+79456955580");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button[type=button]").click();
+        $("[data-test-id=order-success]")
+                .shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
